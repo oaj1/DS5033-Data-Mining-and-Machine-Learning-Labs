24c6032b3dc746d04e8beed335000bf284995a47
Oliver James <31637093+oaj1@users.noreply.github.com>
2024-11-13 13:12:31 -0600

    Add files via upload

--- ch.4 (Supervised Learning)/Lab4.11.py
+++ ch.4 (Supervised Learning)/Lab4.11.py
@@ -1,52 +1,43 @@
-#The dataset SDSS contains 17 observational features and one class feature for 10000 deep sky objects observed by the Sloan Digital Sky Survey. Use sklearn's KNeighborsClassifier() function to perform kNN classification to classify each object by the object's redshift and u-g color.
 
-#Import the necessary modules for kNN classification
-#Create dataframe X with features redshift and u_g
-#Create dataframe y with feature class
-#Initialize a kNN model with k=3
-#Fit the model using the training data
-#Find the predicted classes for the test data
-#Calculate the accuracy score using the test data
+#The diamonds dataset contains the price, cut, color, and other characteristics of a sample of nearly 54,000 diamonds. This data can be used to predict the price of a diamond based on its characteristics. Use sklearn's KNeighborsRegressor() function to predict the price of a diamond from the diamond's carat and table values.
 
-# Import needed packages for classification
-import pandas as pd
-import numpy as np
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.preprocessing import StandardScaler 
+#Import needed packages for regression.
+#Initialize and fit a k-nearest neighbor regression model using a Euclidean distance metric and k=12.
+#Predict the price of a diamond with the user-input carat and table values.
+#Find the distances and indices of the 12 nearest neighbors for the user-input instance
 
-# Import packages for evaluation
-from sklearn.model_selection import train_test_split
-from sklearn import metrics
+# Import needed packages for regression
+import pandas as pd #for dataframe
+import numpy as np #for math
+from sklearn.neighbors import KNeighborsRegressor   # For regression tasks
 
-# Load the dataset
-skySurvey = pd.read_csv('SDSS.csv')
+# Silence warning from sklearn
+import warnings
+warnings.filterwarnings('ignore')
 
-# Create a new feature from u - g
-skySurvey['u_g'] = skySurvey['u'] - skySurvey['g']
+# Input feature values for a sample instance
+carat = float(input())
+table = float(input())
 
-# Create dataframe X with features redshift and u_g
-X = skySurvey[['redshift','u_g']]
+diamonds = pd.read_csv('diamonds.csv')
 
-# Create dataframe y with feature class
-y = skySurvey[['class']]
+# Define input and output features
+X = diamonds[['carat', 'table']]
+y = diamonds['price']
 
-np.random.seed(42)
+# Initialize a k-nearest neighbors regression model using a Euclidean distance and k=12 
+kNNModel = KNeighborsRegressor(n_neighbors=12, metric='euclidean')
 
-# Split data into training and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+# Fit the kNN regression model to the input and output features
+kNNModel = kNNModel.fit(X,y)
 
-# Initialize model with k=3
-skySurveyKnn = KNeighborsClassifier (n_neighbors = 3)
+# Create array with new carat and table values
+Xnew = [[carat, table]]
 
-# Fit model using X_train and y_train
-skySurveyKnn = skySurveyKnn.fit(X_train, np.ravel(y_train))
+# Predict the price of a diamond with the user-input carat and table values
+prediction = kNNModel.predict(Xnew)
+print('Predicted price is', np.round(prediction, 2))
 
-# Find the predicted classes for X_test
-y_pred = skySurveyKnn.predict(X_test)
-
-# Calculate accuracy score
-score =metrics.accuracy_score(y_test, y_pred)
-
-# Print accuracy score
-print('Accuracy score is ', end="")
-print('%.3f' % score)
+# Find the distances and indices of the 12 nearest neighbors for the new instance
+neighbors = kNNModel.kneighbors(Xnew)
+print('Distances and indices of the 12 nearest neighbors are', neighbors)
